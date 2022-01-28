ce4437f5de42273c36018bb3601b28bcb3c4266e
dehanz13 <hansel.danniel@gmail.com>
2022-01-28 00:59:03 -0600

    added http calls

--- src/components/hooks/Counter.tsx
+++ src/components/hooks/Counter.tsx
@@ -13,15 +13,17 @@ export interface Todo {
 export function Counter({ description }: CounterProps) {
 	const [count, setCount] = useState(0);
 	const [todo, setTodo] = useState<Todo>()
+	const [errorMessage, setErrorMessage] = useState('');
+	const [loading, setLoading] = useState(false);
 
-	// cancel timeout using its id
+	// #1. cancel timeout using its id
 	// useEffect(() => {
 	// 	const id = setTimeout(() => {
 	// 		console.log('from cancel timeout', description)
 	// 	}, 3000)
 	// }, [description])
 
-	// cancel time out using boolean
+	// #2. cancel time out using boolean/flag
 	// useEffect(() => {
 	// 	let currentRender = true;
 		
@@ -36,22 +38,83 @@ export function Counter({ description }: CounterProps) {
 	// 		currentRender = false;
 	// 	};
 	// }, [description]);
+	
+	// #3. basic http calls, set response as todo title from jsonplaceholder
+	// useEffect(() => {
+	// 	async function loadData() {
+	// 		const res = await fetch('https://jsonplaceholder.typicode.com/todos/' + description)
+	// 		const json = await res.json();
+	// 		// console.log("fetching...", json);
+	// 		setTodo(json)
+	// 	}
+	// 	loadData();
+	// }, [description]);
+
+	// #4. abort http calls, go to devtools (network tab) and see that we are cancelling the ongoing/pending requests
 	useEffect(() => {
+		let currentRender = true;
+
+		const controller = new AbortController();
+		const signal = controller.signal;
+
+		setLoading(true);
+		setErrorMessage('')
+
 		async function loadData() {
-			const res = await fetch('https://jsonplaceholder.typicode.com/todos/' + description)
-			const json = await res.json();
-			// console.log("fetching...", json);
-			setTodo(json)
+			try {
+				const res = await fetch(
+					// _delay=5000, for each response, we are asking for a delay of 5 seconds on each response to make sure
+					// our browser is not caching any request. 
+					// we are adding a _bustcache parameter with the current date, so we always have new request, no cache (this is only a demo)
+					// in a real application, we would need to have cache
+					`https://jsonplaceholder.typicode.com/todos/${description}?_delay=5000&_bustcache=${new Date()}`,
+					{
+						signal, // this is the signal coming from AbortController.signal
+					}
+				);
+
+				if (!res.ok && currentRender) {
+					// throwing error here because fetch even on 400/500 status code(s) doesn't throw error/exception.
+					// in order to go to the catch block, we need to throw the Error ourselves here, otherwise do some if/else logic
+					throw new Error(`ERROR: ${res.status} status code.`)
+				}
+
+				const json = await res.json();
+
+				if (currentRender) {
+					setTodo(json);
+					setLoading(false);
+					setErrorMessage('');
+				}
+			} catch (err) {
+				if (currentRender) {
+					console.log('ERROR: ', err);
+					setLoading(false);
+					setErrorMessage("Error")
+				}
+			}
 		}
+
 		loadData();
+
+		return () => {
+			currentRender = false;
+			controller.abort(); // aborting the call here...
+		};
 	}, [description]);
+	
+	
+
 	
 
 	return (
 		<div className="">
 			<h5>Desc: {description}</h5>
+			<pre>{JSON.stringify({ loading, error: errorMessage })}</pre>
 			<div className="badge bg-secondary fs-1 text-wrap my-4" style={{width: "10rem"}}>
 				{todo?.title}
+				{todo?.id}
+				{todo?.userId}
 			</div>
 			<div className="col m-4">
 				<button type='button' className="btn btn-danger" onClick={() => setCount(count - 1)}>
